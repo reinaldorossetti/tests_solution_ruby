df39646efe8e2de8128fee139bdc71a172d3f4c8
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2017-05-30 10:16:27 -0300

    Update SendKeys.rb

--- SendKeys.rb
+++ SendKeys.rb
@@ -13,7 +13,7 @@ https://msdn.microsoft.com/en-us/library/8c6yea83.aspx
 # Cria a instancia do Wscript Shell, para poder usar os metodos.
 wsh = WIN32OLE.new('Wscript.Shell')
 
-# Passar o Nome da Janela atual
+# Passar o Nome da Janela atual, um pre-requisito o app deve estah aberto.
 if wsh.AppActivate('*new 3 - Notepad++')
   # Tempo de espera em segundos
   sleep(1)
