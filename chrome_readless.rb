501df6a459708ab765ef91ceb4acaaa572aaaf3a
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2017-06-18 16:14:37 -0300

    Add files via upload

---
+++ chrome_readless.rb
@@ -0,0 +1,13 @@
+require 'selenium-webdriver'
+
+# precisa incluir os matchers e a biblioteca.
+# para fazer o assert com expect
+require 'rspec/expectations'
+include RSpec::Matchers
+
+# instacia o driver do chrome passando os argumentos para executar em modo headless.
+driver = Selenium::WebDriver.for :chrome, :switches => ["--headless", "--disable-gpu"]
+driver.get("http://sandbox.clinicwise.net")
+expect(driver.title).to eq("Wise Clinic")
+puts driver.title
+driver.quit
