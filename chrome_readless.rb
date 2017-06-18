647b934ae39d95dc417503982574831543f3578f
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2017-06-18 16:15:13 -0300

    Update chrome_readless.rb

--- chrome_readless.rb
+++ chrome_readless.rb
@@ -5,7 +5,7 @@ require 'selenium-webdriver'
 require 'rspec/expectations'
 include RSpec::Matchers
 
-# instacia o driver do chrome passando os argumentos para executar em modo headless.
+# instancia o driver do chrome passando os argumentos para executar em modo headless.
 driver = Selenium::WebDriver.for :chrome, :switches => ["--headless", "--disable-gpu"]
 driver.get("http://sandbox.clinicwise.net")
 expect(driver.title).to eq("Wise Clinic")
