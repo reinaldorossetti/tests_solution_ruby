e6745cc73c0d04e90965f463678c184953ce7081
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2017-09-28 19:09:03 -0300

    Update BenchmarkFramework_watir_capybara_selenium.rb

--- BenchmarkFramework_watir_capybara_selenium.rb
+++ BenchmarkFramework_watir_capybara_selenium.rb
@@ -17,11 +17,14 @@ Benchmark.bm do |bm|
   bm.report do
     iterations.times do
       require 'capybara'
-      session = Capybara::Session.new(:selenium)
-      Capybara.default_max_wait_time = 30
+
+      Capybara.register_driver :chrome do |app|
+        Capybara::Selenium::Driver.new(app, :browser => :chrome)
+      end
+
+      session = Capybara::Session.new(:chrome)
       session.visit('http://bit.ly/watir-webdriver-demo')
-      session.fill_in("entry_1000000", :with => 'FrameWorks Testes')
-      session.fill_in("entry_1000000", :with => '1234567890')
+      session.fill_in("entry_1000000", :with => '8wUFjPi5eJRiKwnbAf92FBpgJfSLYUTPGFxtT3mnunTmkJsClS4pMWoSKqqJjP3l1AZbLQHi8wEioEXwr1aylWqdzKhnoFOj5HQKUXJv5iVcUF4ZnvKYfHo4Nlt7zQ5aGLi6gwVsRhStw6L066Mko1otZe2akqKsHlCLlx6QtrPoKffeTdlI4l66D83KQ161iIOSv6km')
       session.select('Ruby', :from => "entry_1000001")
       session.find_by_id("group_1000003_1").click
       session.find_by_id("group_1000004_1").click
@@ -34,11 +37,22 @@ Benchmark.bm do |bm|
   bm.report do
     iterations.times do
       require 'watir'
-      driver = Watir::Browser.start 'bit.ly/watir-webdriver-demo', :firefox
-      driver.text_field(id: "entry_1000000").exists?
-      driver.text_field(id: "entry_1000000").send_keys 'FrameWorks Testes'
-      driver.text_field(id: "entry_1000000").clear
-      driver.text_field(id: "entry_1000000").send_keys '1234567890'
+      driver = Watir::Browser.start 'http://bit.ly/watir-webdriver-demo', :chrome
+      driver.text_field(id: "entry_1000000").send_keys '8wUFjPi5eJRiKwnbAf92FBpgJfSLYUTPGFxtT3mnunTmkJsClS4pMWoSKqqJjP3l1AZbLQHi8wEioEXwr1aylWqdzKhnoFOj5HQKUXJv5iVcUF4ZnvKYfHo4Nlt7zQ5aGLi6gwVsRhStw6L066Mko1otZe2akqKsHlCLlx6QtrPoKffeTdlI4l66D83KQ161iIOSv6km'
+      driver.select_list(id: "entry_1000001").select 'Ruby'
+      driver.radio(id: "group_1000003_1").click
+      driver.checkbox(id: "group_1000004_1").click
+      driver.button(name: 'submit').click
+      driver.quit
+    end
+  end
+
+  puts "'watir-webdriver-performance"
+  bm.report do
+    iterations.times do
+      require 'watir-webdriver'
+      driver = Watir::Browser.start 'http://bit.ly/watir-webdriver-demo', :chrome
+      driver.text_field(id: "entry_1000000").send_keys '8wUFjPi5eJRiKwnbAf92FBpgJfSLYUTPGFxtT3mnunTmkJsClS4pMWoSKqqJjP3l1AZbLQHi8wEioEXwr1aylWqdzKhnoFOj5HQKUXJv5iVcUF4ZnvKYfHo4Nlt7zQ5aGLi6gwVsRhStw6L066Mko1otZe2akqKsHlCLlx6QtrPoKffeTdlI4l66D83KQ161iIOSv6km'
       driver.select_list(id: "entry_1000001").select 'Ruby'
       driver.radio(id: "group_1000003_1").click
       driver.checkbox(id: "group_1000004_1").click
@@ -51,23 +65,28 @@ Benchmark.bm do |bm|
   bm.report do
     iterations.times do
       require 'selenium-webdriver'
-      driver = Selenium::WebDriver.for :firefox
+      driver = Selenium::WebDriver.for :chrome
       driver.navigate.to 'http://bit.ly/watir-webdriver-demo'
 
       def wait_for(seconds=30)
         Selenium::WebDriver::Wait.new(:timeout => seconds).until {yield}
       end
 
-      wait_for {driver.find_element(id: "entry_1000000")}
-      driver.find_element(id: "entry_1000000").send_keys 'FrameWorks Testes'
-      driver.find_element(id: "entry_1000000").clear
-      driver.find_element(:id, "entry_1000000").send_keys '1234567890'
+      elem = wait_for {driver.find_element(id: "entry_1000000")}
+      elem.send_keys '8wUFjPi5eJRiKwnbAf92FBpgJfSLYUTPGFxtT3mnunTmkJsClS4pMWoSKqqJjP3l1AZbLQHi8wEioEXwr1aylWqdzKhnoFOj5HQKUXJv5iVcUF4ZnvKYfHo4Nlt7zQ5aGLi6gwVsRhStw6L066Mko1otZe2akqKsHlCLlx6QtrPoKffeTdlI4l66D83KQ161iIOSv6km'
+
       dropDownMenu = wait_for {driver.find_element(id: "entry_1000001")}
       options = Selenium::WebDriver::Support::Select.new(dropDownMenu)
       options.select_by(:text, "Ruby")
-      driver.find_element(id: "group_1000003_1").click
-      driver.find_element(id: "group_1000004_1").click
-      driver.find_element(:name, 'submit').click
+
+      elem = wait_for {driver.find_element(id: "group_1000003_1")}
+      elem.click
+
+      elem = wait_for {driver.find_element(id: "group_1000004_1")}
+      elem.click
+
+      elem = wait_for {driver.find_element(:name, 'submit')}
+      elem.click
       driver.quit
     end
   end
