6b8f3e80ea1e90d42f3fc2744bba5c85492d074d
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-01-21 15:59:11 -0200

    Create firefox_profile_download.rb

---
+++ selenium_tests/firefox_profile_download.rb
@@ -0,0 +1,16 @@
+
+    profile = Selenium::WebDriver::Firefox::Profile.new
+    profile.native_events = true
+    $download_dir_atual = File.expand_path File.dirname(__FILE__) + '../../downloads'
+    profile['browser.download.folderList'] = 2
+    profile['pdfjs.disabled'] = true
+    profile['browser.download.dir'] = $download_dir_atual.gsub("/", "\\\\") #"C:\\GitSpread\\ProjetoDasa\\features\\downloads"
+    profile['browser.helperApps.neverAsk.saveToDisk']= "application/pdf, application/x-pkcs7, text/xml, text/csv, text/plain, text/log, application/zip, application/x-gzip, text/html, application/excel, application/vnd.ms-excel, application/x-excel, application/x-msexcel"
+
+    # limpar o cache
+    profile['browser.cache.disk.enable'] = false
+    profile['browser.cache.memory.enable'] = false
+    profile['browser.cache.offline.enable'] = false
+    profile['network.http.use-cache'] = false
+
+    Capybara::Selenium::Driver.new(app, :browser => :firefox, profile: profile, :marionette => true)
