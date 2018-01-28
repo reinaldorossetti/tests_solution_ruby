173710df678557b6e1fa055c1e3f28a99fb722a1
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-01-28 14:42:27 -0200

    Add files via upload

---
+++ brincando_com_descontos.rb
@@ -0,0 +1,50 @@
+# Simulando uma lista de uma base, item, valor, quant_d, valor_desconto
+$array_m = [["A", 50, 3, 130],
+            ["B", 30, 0, 0],
+            ["C", 20, 0, 0],
+            ["D", 15, 5, 50]
+]
+$itens = $array_m.transpose[0]
+$values = $array_m.transpose[1]
+
+# convert string to array of itens.
+def to_array(string)
+  string.split('').to_a if string.is_a?(String)
+  to_quantity(string)
+end
+
+# get quantity of itens in list.
+def to_quantity(array)
+  $array_quant = []
+  $itens.each {|item| $array_quant << [item, array.count(item)] if array.count(item) > 0}
+end
+
+def apply_values
+  dic_values = Hash[$itens.zip($values)]
+  $array_quant.each_with_index {|array, index_q|
+    array.each_with_index {|item, index_i|
+      dic_values.each {|key, value|
+        $array_quant[index_q].insert(1, value*(array[1])) if key == item
+      }
+    }
+  }
+end
+
+def apply_desc
+  # pegar os itens que estão em desconto.
+  itens = $array_m.reject {|array| array[2]==0}
+  p itens
+  $array_quant.each_with_index {|array, index_q|
+    array.each_with_index {|item, index_i|
+      itens.each_with_index {|array2|
+        if array2[0]==array[0] and array[2]>=array2[2]
+
+        end
+      }
+    }
+  }
+end
+
+array = to_array("AAABCDDD")
+p apply_values
+p apply_desc
