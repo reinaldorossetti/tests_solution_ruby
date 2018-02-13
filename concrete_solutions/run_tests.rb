2c721ca888937733f249a5be92657745f0393063
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-02-13 12:33:25 -0200

    Deixando mais organizado o teste unitário.
    Deixando mais organizado o teste unitário. Removi a sum pra colocar o .reduce(:+), dependendo da versão do ruby estava gerando instabilidade, ou seja uma hora funcionava e outra não.

--- concrete_solutions/run_tests.rb
+++ concrete_solutions/run_tests.rb
@@ -1,21 +1,36 @@
 # encoding: UTF-8
 require_relative 'desconto'
 
-include DataBase
+class UnitTest
 
-# adicionando novo valor.
-p values(["E",10, 5, 40])
-p values(["E",10, 5, 40]) # teste com valores repetidos.
-test = Account.new
+  include DataBase
 
-list = [[0, ""],[50, "A"],[80, "AB"], [115, "CDBA"], [100, "AA"], [130, "AAA"],[180, "AAAA"],
-[230, "AAAAA"], [260,"AAAAAA"], [160, "AAAB"], [175, "AAABB"],[190, "AAABBD"], [190, "DABABA"]]
+  def initialize
+    @test = Account.new
+  end
 
-new_array = list.map{|array| array.reverse}
-new_array.each{ |array|
-	to_array(array[0]) # envia os itens para calcular o valor.
-  	p test.apply_values # sem desconto.
-  	p result = test.apply_desc # com desconto.
-	p comparacao = (result.map{|arr| arr[1] }).sum==array[1] # meu teste unitario comparando com o valor do array.
-	raise "O Teste Unitário Falhou!!!" unless comparacao
-}
+  # adicionando novo valor.
+  def add_values(list)
+    p values(list)
+  end
+
+  # faz o assert da lista de dados.
+  def assert_values(list)
+    new_array = list.map {|array| array.reverse}
+    new_array.each {|array|
+      to_array(array[0]) # envia os itens para calcular o valor.
+      p @test.apply_values # sem desconto.
+      p result = @test.apply_desc # com desconto.
+      result.empty? ? list = 0 : list = (result.map {|arr| arr[1]}).reduce(:+)
+      p comparacao = (list)==array[1] # meu teste unitario comparando com o valor do array.
+      raise "O Teste Unitário Falhou!!!" unless comparacao
+    }
+  end
+end
+
+test = UnitTest.new
+test.add_values(["E", 10, 5, 40])
+list = [[0, ""], [50, "A"], [80, "AB"], [115, "CDBA"], [100, "AA"], [130, "AAA"], [180, "AAAA"],
+        [230, "AAAAA"], [260, "AAAAAA"], [160, "AAAB"], [175, "AAABB"], [190, "AAABBD"], [190, "DABABA"]]
+
+test.assert_values(list)
