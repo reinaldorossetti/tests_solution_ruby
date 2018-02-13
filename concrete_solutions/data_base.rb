f65cd53afb230369bc10892b6498281fcf8f8fd7
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-02-13 12:35:02 -0200

    Reject alterado pra forma mais reduzida.
    Reject alterado pra forma mais reduzida.

--- concrete_solutions/data_base.rb
+++ concrete_solutions/data_base.rb
@@ -1,18 +1,21 @@
+# encoding: UTF-8
+
 module DataBase
 
+  # Simulando uma lista de uma base, item, valor, quant_d, valor_desconto
   def values(new_value)
-    # Simulando uma lista de uma base, item, valor, quant_d, valor_desconto
     $array_m = [["A", 50, 3, 130],
                 ["B", 30, 2, 45],
                 ["C", 20, 0, 0],
                 ["D", 15, 0, 0]]
-                
+
     $array_m.push(new_value).uniq! # insere e remove valores repetidos.
     $itens = $array_m.transpose[0]
     $values = $array_m.transpose[1]
-    $desc_q_min = ($array_m.transpose[2].reject!{|x| x==0}).min
+    $desc_q_min = $array_m.transpose[2].reject!(&:zero?).min
     $array_m
   end
+
   # convert string to array of itens.
   def to_array(string)
     string.gsub!(/[^A-Za-z]/, '') if string.is_a?(String) # somente letras se for str.
@@ -25,8 +28,7 @@ module DataBase
     $array_quant = []
     $itens.each {|item| $array_quant << [item, array.count(item)] if array.count(item) > 0}
     $itens_em_desc = $array_m.reject {|array| array[2]==0}
-    $prod_com_d,$prod_sem_d = $array_quant.partition{|array| array[1]>=$desc_q_min}
+    $prod_com_d, $prod_sem_d = $array_quant.partition {|array| array[1]>=$desc_q_min}
     $array_quant
   end
-  
 end
