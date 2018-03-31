519018f567ea1af066b587c11fb69f913b9f3a3d
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-03-31 11:35:18 -0300

    usando a classe log no ruby.
    usando a classe log no ruby.

--- logger.rb
+++ logger.rb
@@ -1,16 +1,20 @@
 require 'logger'
-require 'date'
 
+# Ler ou cria o arquivo .log.
 file = File.open('Log_file_#{date_time.to_s}.log', 'w+')
+
+# instancia a classe Logger.
 logger = Logger.new(file)
-logger.level = Logger::ERROR
+
+# Define somente os erros no log.
+logger.level = Logger::ERROR 
+
+# Alterando para o formato brasileiro.
 logger.datetime_format = '%d-%m-%Y %H:%M:%S'
 
+# como usar a classe log do ruby na funcao.
 begin
 	t = 4/0
 rescue => ex
-	p ex
 	logger.error(ex)
 end
-
-
