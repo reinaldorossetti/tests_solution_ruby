8408de815cd05856b089273c06c8f3e5e9a4bfd5
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-06-23 20:02:38 -0300

    Update form_selectors.rb

--- jquery_uso/form_selectors.rb
+++ jquery_uso/form_selectors.rb
@@ -7,13 +7,14 @@ end
 
 session = Capybara::Session.new(:chrome)
 session.visit('https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onmouseover')
-
 session.driver.browser.switch_to.frame ("iframeResult")
-elem = session.find("img[onmouseover*=bigImg]").hover
-
+elem = session.find("img[onmouseover*=bigImg]")
+#elem.hover
+dir_atual = File.expand_path File.dirname(__FILE__)
+p dir_atual
 query = elem.instance_variable_get(:@query).locator
-js = File.read('jquery.min.js')
+js = File.read("#{dir_atual}/jquery.min.js")
 session.execute_script(js)
-session.execute_script("$('#{query}').hide()")
+session.execute_script("$('#{query}').trigger(\"mouseover\")")
 sleep 10 # somente para ver o elemento sumindo.
 session.close
