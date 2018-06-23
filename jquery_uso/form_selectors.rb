9d4c44ecd2c08a083926241b0e7eb47bffb78fd6
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-06-23 20:42:33 -0300

    Update form_selectors.rb

--- jquery_uso/form_selectors.rb
+++ jquery_uso/form_selectors.rb
@@ -5,16 +5,20 @@ Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, :browser => :chrome )
 end
 
-session = Capybara::Session.new(:chrome)
-session.visit('https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onmouseover')
-session.driver.browser.switch_to.frame ("iframeResult")
-elem = session.find("img[onmouseover*=bigImg]")
-#elem.hover
-dir_atual = File.expand_path File.dirname(__FILE__)
-p dir_atual
-query = elem.instance_variable_get(:@query).locator
-js = File.read("#{dir_atual}/jquery.min.js")
-session.execute_script(js)
-session.execute_script("$('#{query}').trigger(\"mouseover\")")
+$session = Capybara::Session.new(:chrome)
+$session.visit('https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onmouseover')
+$session.driver.browser.switch_to.frame ("iframeResult")
+
+def trigger_event(elem, evento)
+  dir_atual = File.expand_path File.dirname(__FILE__)
+  query = elem.instance_variable_get(:@query).locator
+  js = File.read("#{dir_atual}/jquery.min.js")
+  $session.execute_script(js)
+  $session.execute_script("$(arguments[0]).trigger(\"#{evento}\")", elem)
+end
+
+elem = $session.find("img[onmouseover*=bigImg]")
+trigger_event(elem, "mouseover")
+
 sleep 10 # somente para ver o elemento sumindo.
-session.close
+$session.close
