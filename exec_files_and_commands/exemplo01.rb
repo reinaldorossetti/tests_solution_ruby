d82f8318d77bdb0859d9f80990ab6b1e61b63eeb
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-07-15 15:53:08 -0300

    Update exemplo01.rb

--- exec_files_and_commands/exemplo01.rb
+++ exec_files_and_commands/exemplo01.rb
@@ -1,6 +1,6 @@
 $project = (File.expand_path File.dirname(__FILE__) + "/features/reports/").gsub!('/', '//')
-p json =  File.join($project, "//jsons//")
-p report =  File.join($project, "//Motion_Report_Global")
+p path_jsons =  File.join($project, "//jsons//")
+p path_report =  File.join($project, "//Motion_Report_Global")
 
 # Start a new CMD shell and terminates
-p system("cmd /c report_builder -s \"#{json}\" -o \"#{report}\"")
+p system("cmd /c report_builder -s \"#{path_jsons}\" -o \"#{path_report}\"")
