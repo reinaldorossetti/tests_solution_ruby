f4d528ca0408bbe40087b1af37c662c920b97aef
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-07-15 15:58:38 -0300

    Update exemplo01.rb

--- exec_files_and_commands/exemplo01.rb
+++ exec_files_and_commands/exemplo01.rb
@@ -2,5 +2,5 @@ $reports = (File.expand_path File.dirname(__FILE__) + "/features/reports/").gsub
 p path_jsons =  File.join($reports, "//jsons//")
 p report_file =  File.join($reports, "//Motion_Report_Global")
 
-# Start a new CMD shell and terminates
+# Start a new CMD shell windows and terminates.
 p system("cmd /c report_builder -s \"#{path_jsons}\" -o \"#{report_file}\"")
