dc9707017092a35d301e88d6ac0efadc9d158271
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2020-02-22 14:48:59 -0300

    mudando o nome da instancia session
    mudando o nome da instancia session para browser, para ficar mais fácil o exemplo.

--- jquery_uso/form_selectors.rb
+++ jquery_uso/form_selectors.rb
@@ -5,20 +5,20 @@ Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, :browser => :chrome )
 end
 
-$session = Capybara::Session.new(:chrome)
-$session.visit('https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onmouseover')
-$session.driver.browser.switch_to.frame ("iframeResult")
+$browser = Capybara::Session.new(:chrome)
+$browser.visit('https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onmouseover')
+$browser.driver.browser.switch_to.frame ("iframeResult")
 
 def trigger_event(elem, evento)
-  dir_atual = File.expand_path File.dirname(__FILE__)
+  dir_atual = File.expand_path File.dirname(__FILE__) # pega o caminho do arquivo
   query = elem.instance_variable_get(:@query).locator
-  js = File.read("#{dir_atual}/jquery.min.js")
-  $session.execute_script(js)
-  $session.execute_script("$(arguments[0]).trigger(\"#{evento}\")", elem)
+  js = File.read("#{dir_atual}/jquery.min.js") # ler o arquivo jquery
+  $browser.execute_script(js)
+  $browser.execute_script("$(arguments[0]).trigger(\"#{evento}\")", elem)
 end
 
-elem = $session.find("img[onmouseover*=bigImg]")
+elem = $browser.find("img[onmouseover*=bigImg]")
 trigger_event(elem, "mouseover")
 
 sleep 10 # somente para ver o elemento sumindo.
-$session.close
+$browser.close
